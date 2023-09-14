08cbab92cef182d13c221981394100349baea4f2
Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>
2023-09-14 12:16:35 -0400

    Initial commit
    Signed-off-by: Vignesh Saravanakumar <vignesh.saravanakumar.vs@gmail.com>

---
+++ Day2/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+
+void method1(int number) {
+    for (int x = 0; x < number; x++) {
+        if (x % 2 == 0) {
+            cout << x << std::endl;
+        }
+    }
+
+}
+
+char* method2(string s) {
+    // return the array as a char array
+    char* array = new char[s.size()];
+    for (int x = 0; x < s.size(); x++) {
+        array[x] = s[x];
+    }
+    return array;
+}
+
+bool** method3(bool** array, bool flip){
+     // if true then flip all the values
+    if (flip) {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                array[x][y] = !array[x][y];
+            }
+        }
+        return array;
+    }
+    else{
+        return array;
+    }
+
+}
+
+
+
+int main() {
+
+
+
+    // Method 1
+    method1(10);
+
+    // Method 2
+    method2("Hello World");
+    for (int x = 0; x < 11; x++) {
+        cout << method2("Hello World")[x] << std::endl;
+    }
+
+    // Method 3
+    bool** array = new bool* [3];
+    for (int x = 0; x < 3; x++) {
+        array[x] = new bool[3];
+        for (int y = 0; y < 3; y++) {
+            array[x][y] = true;
+        }
+    }
+    // print the result of method3(array, true);
+    method3(array, true);
+    for (int x = 0; x < 3; x++) {
+        cout << "[";
+        for (int y = 0; y < 3; y++) {
+            cout << array[x][y] << ", ";
+        }
+        cout << "]" << std::endl;
+    }
+
+    method3(array, false);
+    for (int x = 0; x < 3; x++) {
+        cout << "[";
+        for (int y = 0; y < 3; y++) {
+            cout << array[x][y] << ", ";
+        }
+        cout << "]" << std::endl;
+    }
+
+
+
+    return 0;
+}
